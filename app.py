d5b1440db6498456a8abc6ce5d3a9a9f507ab1f6
Siriusly-Byte <beyuanhui@gmail.com>
2026-07-24 13:57:43 +0800

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-
-BASE = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, BASE)
-sys.path.insert(0, os.path.join(BASE, 'backend'))
-
-from backend.app import create_app
-app = create_app('prod')
