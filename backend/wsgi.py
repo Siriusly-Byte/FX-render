ea7c0176de816bd7abc4f199747fe30b9f2c4353
Siriusly-Byte <beyuanhui@gmail.com>
2026-07-24 11:47:40 +0800

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,12 +1,6 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
 from app import create_app
 
-config_name = 'prod' if os.environ.get('FLASK_ENV') == 'production' else 'dev'
-app = create_app(config_name)
+app = create_app('prod')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run()
